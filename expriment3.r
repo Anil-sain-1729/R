353c8ea728c689ea69f7928f4aa1018c947416b6
Anil_sain_1729 <asain4773@gmail.com>
2026-03-01 13:14:28 +0530

    Add files via upload

---
+++ expriment3.r
@@ -0,0 +1,20 @@
+# Vector
+v1<- c(10,20,30,40)
+v2<- c(2,4,6,8)
+
+cat(v1)
+cat(v2)
+
+cat("SUM :"v1+v2)
+cat("Sum :"v1-v2)
+cat("Mul :"v1*v2)
+cat("Div :"v1/v2)
+
+# Matrix
+mat<-matrix(c(2,4,6,8,10,12),nrow = 3, ncol = 2)
+vec_col <- c(1, 2)
+vec_row <- c(1, 2, 3)
+vec_mul <- c(1, 2)
+cat("Matrix (3,2) :")
+mat
+
