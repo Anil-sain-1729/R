6cd91129c5e9bf212f26bb3080e4222188ad4de9
Anil_sain_1729 <asain4773@gmail.com>
2026-03-19 00:34:39 +0530

    mane

---
+++ Functions.r
@@ -0,0 +1,17 @@
+# R Built-in Functions
+
+x<- -5.6
+print(abs(x))
+print(floor(x))
+print(ceiling(x))
+
+print(sqrt(45))
+
+y<-c(1.5,5.6,8.7)
+print(trunc(y))
+
+z<-5
+print(sin(5))
+print(log(z))
+print(exp(z))
+
