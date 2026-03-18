8a537c0f7d8d4f7f6934ca468043500fc9da6571
Anil_sain_1729 <asain4773@gmail.com>
2026-03-19 00:35:37 +0530

    mane

---
+++ Operators.r
@@ -0,0 +1,42 @@
+## Operators in R
+## Arithmetic operator   + - * / %% % ^ %/%
+## Relational     < > == != !< !> =< =>
+## Logical     & | ! &&  ||
+## Assignment    
+
+a<-7.5
+b<-2
+a+b
+a-b
+a*b
+a/b
+a%%b  #Reminder
+#a%/b% # Quotient
+a^b
+
+#vectors
+c<-c(8,6,4)
+d<-c(2,4,6)
+c+d
+c-d
+c*d
+c/d
+c%%d
+c^d
+
+# Relational 
+a<-7.5
+b<-2
+a<b
+a>b
+a=b
+
+## Logical 
+e<-c(3.5,TRUE)
+f<-c(2,TRUE)
+e&f
+e|f
+a&&b
+a||b
+!e
+
