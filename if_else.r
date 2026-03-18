ec3c3ec198daa79640cf6138d403f5214d84364f
Anil_sain_1729 <asain4773@gmail.com>
2026-03-19 00:34:58 +0530

    mane

---
+++ if_else.r
@@ -0,0 +1,17 @@
+# R if-else statements
+
+x<-25
+if(is.integer(x)){
+  print("x is integer")
+}else{
+  print("x is not integer")
+}
+
+y<-c("Hardwork","is","the","key","of","success")
+y
+if("key"%in%y){
+  print("key is found in our vector")
+}else{
+  print("key is not found in our vextor")
+}
+
