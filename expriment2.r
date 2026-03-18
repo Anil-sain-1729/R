47277471e4331f2177ebac76e7fd65614cf04902
Anil_sain_1729 <asain4773@gmail.com>
2026-03-19 00:33:16 +0530

    mane

---
+++ expriment2.r
@@ -0,0 +1,82 @@
+# Numeric data type
+a<-23
+a
+class(a)
+
+b<-48.5
+b
+class(b)
+
+# Integer data type
+c<-23L
+c
+class(c)
+
+# Complix data type
+d<- 5+5i
+d
+class(d)
+
+# Character data type
+e<-"Apple"
+e
+class(e)
+
+# Logical data type
+f<-TRUE
+f
+class(f)
+
+
+# Multiple variables
+g<-h<-i<-j<-10
+g
+h
+i
+j
+
+
+## Operators
+
+# Aithmetic 
+k<-15
+l<-2
+k+l
+k-l
+k*l
+k/l
+k^l
+k%%l
+k%/%l
+
+# Assignment
+m<-50
+m
+51->n
+n
+
+# Comparison
+o<-10
+p<-20
+o==p
+o!=p
+o>p
+o<p
+o<=p
+o>=p
+
+# Logical
+q<-TRUE
+r<-FALSE
+q&r
+q|r
+q&&r
+q||r
+!q
+!r
+
+# Miscellaneous
+s<-1:10
+s
+3%in%s
+12%in%s
