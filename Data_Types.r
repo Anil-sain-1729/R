cbae7aa07dffef2727e3b7ec105f09e8522ef8e2
Anil_sain_1729 <asain4773@gmail.com>
2026-03-19 00:31:57 +0530

    mane

---
+++ Data_Types.r
@@ -0,0 +1,26 @@
+## Data Types in R 
+## logical , numeric , integer , complex , character , row
+## numeric= 12,1232,-24,12.345,-26,1234
+## integer = 36L
+## complex = 5+3i
+## logical =TRUE,FALSE
+## character = 'a',"hello R","12.234","@#$dkje"
+
+num<-10
+class(num)
+typeof(num)
+num
+
+num2<-10L
+class(num2)
+typeof(num2)
+
+num3<-5-3i
+class(num3)
+typeof(num3)
+
+logi<-TRUE
+class(logi)
+
+char<-"FSHJKLR"
+class(char)
