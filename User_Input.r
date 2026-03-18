68601fe72c9e80e4a4b24db5f6c5de77a6acd672
Anil_sain_1729 <asain4773@gmail.com>
2026-03-19 00:37:06 +0530

    mane

---
+++ User_Input.r
@@ -0,0 +1,10 @@
+# R Take Input fore User
+
+name<-readline(prompt = "Enter your name: ")
+age<-readline(prompt = "Enter your age: ")
+print(paste("hello My Name is: ",name,"and my age is: ",age))
+
+paste("hello","233","ram")
+paste("hello","233","ram",sep="_")
+
+paste0("hello","233","ram")
