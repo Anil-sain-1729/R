0b1a95c2c8a549dc70a3e95dfe82107c397b566d
Anil_sain_1729 <asain4773@gmail.com>
2026-03-19 00:31:34 +0530

    mane

---
+++ Converting_Data_Types.r
@@ -0,0 +1,21 @@
+## Converting Data Types in R
+
+num1<-as.numeric(26L)
+num1
+num2<-as.numeric(25-56i)
+num2
+num3<-as,numeric(TRUE)
+num3
+num4<-as.numeric("asdfgfj12345")
+num4
+num5<-as.numeric("12345678")
+num5
+
+int1<-as.integer(53.3456)
+int1
+int2<-as.integer(46-56i)
+int2
+int3<-as,integer(TRUE)
+int3
+int4<-as.integer("12345")
+int4
